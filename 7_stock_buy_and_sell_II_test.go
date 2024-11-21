40559ec42547dc8a1daa84feb8af5458951ade46
Harith <harith@a5labs.co>
2024-11-21 13:58:16 +0530

    chore: day 7

---
+++ 7_stock_buy_and_sell_II_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+)
+
+type TestCaseStockBuyAndSellII struct {
+	name           string
+	input          []int
+	expectedOutput int
+}
+
+func TestMaxProfit(t *testing.T) {
+	tcs := []TestCaseStockBuyAndSellII{
+		{
+			name:           "TC01",
+			input:          []int{100, 180, 260, 310, 40, 535, 695},
+			expectedOutput: 865,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			resp := MaxProfit(tc.input)
+			if tc.expectedOutput != resp {
+				t.Fatalf("expected %v but got %v", tc.expectedOutput, resp)
+			}
+		})
+	}
+}
